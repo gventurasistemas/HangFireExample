01123a963f1619e90c3f9d1e4e80c272fa309b5b
GelsonVentura-github <gelsonventuragomes@gmail.com>
2020-01-12 10:52:21 -0300

    Initial Commit

---
+++ HangFireExample/Controllers/JobsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangFireExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobsController : ControllerBase
+    {
+        public string Get()
+        {
+            Console.WriteLine($"Request: {DateTime.Now}");
+            var jobFireForget = BackgroundJob.Enqueue(() => Debug.WriteLine($"Fire and forget: {DateTime.Now}"));
+            var jobDelayed = BackgroundJob.Schedule(() => Debug.WriteLine($"Aguarde (Delay): {DateTime.Now}"), TimeSpan.FromSeconds(30));
+            BackgroundJob.ContinueWith(jobDelayed, () => Debug.WriteLine($"Continuação: {DateTime.Now}"));
+            RecurringJob.AddOrUpdate(() => Debug.WriteLine($"Recorrente: {DateTime.Now}"), Cron.Minutely);
+            return "Jobs criados com sucesso!";
+        }
+    }
+}
---
+++ HangFireExample/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HangFireExample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);                      
+            services.AddHangfire(x =>
+                                     x.UseSqlServerStorage(Configuration.GetConnectionString("HangFireConnection")));
+            services.AddHangfireServer();
+           
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHangfireDashboard();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+           
+        }
+    }
+}
